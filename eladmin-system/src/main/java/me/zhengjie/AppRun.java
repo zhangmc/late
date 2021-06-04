c0478dc4cd155429f0d356967adc587e6101ca54
ZMC <zmc0927@gmail.com>
2021-06-04 16:42:26 +0800

    新增模块

--- eladmin-system/src/main/java/me/zhengjie/AppRun.java
+++ eladmin-system/src/main/java/me/zhengjie/AppRun.java
@@ -20,10 +20,13 @@ import me.zhengjie.annotation.rest.AnonymousGetMapping;
 import me.zhengjie.utils.SpringContextHolder;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.bind.annotation.RestController;
@@ -40,6 +43,9 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @EnableTransactionManagement
 @EnableJpaAuditing(auditorAwareRef = "auditorAware")
+@ComponentScan(basePackages ={"me.zhengjie","com.zmc"})
+@EnableJpaRepositories({"me.zhengjie.**.repository","com.zmc.**.repository"})
+@EntityScan({"com.zmc.*","me.zhengjie.*"})
 public class AppRun {
 
     public static void main(String[] args) {
